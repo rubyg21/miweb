856a0095e540c0c9e4c935bc2ce74ad9e328f6f0
Rubens Galarraga <rgalarraga121@gmail.com>
2022-03-22 06:51:59 -0300

    agregamos el js y el index a nuetro repositorio

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const letras = ['a','b','c','d']
+
+const resultado = letras.filter(letra => letra === 'a' || letra === 'b')
+// console.log(resultado);
+
+
+const numeros = [1,2,3,4,5,6,7,8,9]
+const resultado1 = numeros.filter(numero => numero > 8 || numero < 2)
+
+
+console.log(resultado1)
